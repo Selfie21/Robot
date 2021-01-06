80088e2f55d1e6f3cec575054f9b78eaefa8842b
Selfie <pierre.brosemer@gmail.com>
2021-01-06 22:53:10 +0100

    added blink function

---
+++ blink/blink.ino
@@ -0,0 +1,16 @@
+/*
+	Simple Blink Function
+*/
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
